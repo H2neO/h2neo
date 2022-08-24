05f6de831daae7b57fefdc50610413e141e527d4
Cory-Laptop <cslam7@uw.edu>
2022-08-23 18:56:09 -0700

    removed old files and commented main code

--- h2neo_monitor/filter.cpp
+++ h2neo_monitor/filter.cpp
@@ -1,5 +1,6 @@
 #include "filter.h"
 
+// Swaps the values of p and q
 void swap(float *p, float *q){
   float t;
   
@@ -8,6 +9,7 @@ void swap(float *p, float *q){
   *q = t;
 }
 
+// Calculate and returns median of float array of given len
 float calcMedian(float *dataPtr, int len){
   float tempData[len];
   float median;
@@ -29,14 +31,20 @@ float calcMedian(float *dataPtr, int len){
 // The actual drop detection calculations
 void thresholding(int index, float *inSignalPtr, float *medFilterPtr, float threshold, int *triggerPtr, bool *dropFlagPtr) {
 
+    // Initialize trigger flag if pass filter
+    // Supposedly accounts for changing baseline as output voltage levels decrease with battery, but not tested **TODO**
     if ((*(inSignalPtr + index) < *(medFilterPtr)*(1-threshold)) && (*dropFlagPtr == 0)) {
            *triggerPtr = 1;
     } else {
+
+        // If the trigger flag is tripped, then set drop flag
+        // Trigger flag exists separately from drop flag to avoid multiple drops being detected within the same drop
         if(*triggerPtr == 1){
             *dropFlagPtr = 1; // dropFLG triggers when incrementing # of peaks
         }
         *triggerPtr = 0;
     }
-        
+
+    // Update median filter with the calculated median from last index + 1 ADC values
     *(medFilterPtr + index) = calcMedian(inSignalPtr, index+1);
 }
